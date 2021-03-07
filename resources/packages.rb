2012a275568a17bb3b1c73fca04e5d35ae7d542c
Benjamin M. Hughes <bmhughes@bmhughes.co.uk>
2021-03-07 18:00:08 +0000

    Refactor package and service resources
    Signed-off-by: Ben Hughes <bmhughes@bmhughes.co.uk>

--- resources/packages.rb
+++ resources/packages.rb
@@ -1,19 +1,14 @@
 include Iptables::Cookbook::Helpers
 
 property :packages, Array,
-  default: lazy { package_names },
-  description: 'The packages to install for iptables'
+          default: lazy { package_names },
+          description: 'The packages to install for iptables'
 
-action :install do
-  package 'iptables' do
-    package_name new_resource.packages
-    action :install
-  end
-end
-
-action :remove do
-  package 'iptables' do
-    package_name new_resource.packages
-    action :remove
+%i(install purge reconfig remove upgrade).each do |service_action|
+  action service_action do
+    package 'iptables' do
+      package_name new_resource.packages
+      action service_action
+    end
   end
 end
